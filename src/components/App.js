ef99856f5ca2b897dea3ea26618f59c39a240d64
Your Name <you@example.com>
2020-06-02 20:18:31 -0400

    test

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,7 @@ import React, { Component } from 'react';
 import Home from './Home.js';
 import { Link } from 'react-router-dom';
 
-<<<<<<< HEAD
-class App extends Component {
-=======
 export default class App extends Component {
->>>>>>> 2b003cb12fb3616c7941adf1f56394ee7b26b8c1
 	constructor() {
 		super();
 		this.state = {
@@ -16,13 +12,8 @@ export default class App extends Component {
 	render() {
 		return (
 			<div className="Page-wrapper">
-<<<<<<< HEAD
-				<Link to="/home">Go Back</Link>
-				<h2>ada app</h2>
-=======
 				<Link to="/home">Go to Other Page</Link>
 				<h2>This is the home page</h2>
->>>>>>> 2b003cb12fb3616c7941adf1f56394ee7b26b8c1
 				<Home />
 			</div>
 		);
